1c5fc6072f73b9d827d008b308f619f28473612c
tomsza123 <szablak.tomek@gmail.com>
2021-02-07 21:07:53 +0100

    Delete src directory

--- src/saveToLocalStorage.js
+++
@@ -1,8 +0,0 @@
-const saveToLocalStorage = (data,place) => {//pushing e.g book to table
-    let existing = [];
-    existing = JSON.parse(localStorage.getItem(place)) || [] ;
-    existing.unshift(data);
-    localStorage.setItem(place, JSON.stringify(existing));
-};
-
-module.exports = saveToLocalStorage;
